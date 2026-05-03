653998a9655bc7ed3b718f8468f7dcddcdbd6054
Morten Gislund <ext-Morten.Gislund@ramboll.com>
2026-05-03 15:12:34 +0200

    added config

--- gi5lund_dk/next.config.ts
+++ gi5lund_dk/next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /**
+   * Enable static exports for the App Router.
+   *
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
+  output: "export",
+
+  /**
+   * Disables image optimization, as it is not supported with static exports.
+   * The GitHub Action (`configure-pages`) also does this, but it's good practice to have it here.
+   */
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
